bcdd78974215dbccab301df9c92ef3dfc2e1a4ba
inamuu <8310973+inamuu@users.noreply.github.com>
2020-09-08 22:20:18 +0900

    :shower: Delete sample app to recreate app

--- test_app/app/controllers/top_controller.rb
+++
@@ -1,5 +0,0 @@
-class TopController < ApplicationController
-  def index
-    @message = "おはようございます！"
-  end
-end
--- test_app/config/initializers/generators.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.generators do | g |
-  g.helper false
-  g.asses false
-  g.skip_routes true
-  g.test_framework false
-end
